8d125e62928d85c10ac6f3194dd86f2a5b9b9370
seongs1024 <seongs1024@gmail.com>
2020-11-27 22:49:55 +0900

    Add SidePanel (ref. https://github.com/emilk/egui/blob/8c6ccd1f7ca4306cd0581c0e967dc9713c851a5e/egui/src/demos/demo_windows.rs#L105)

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 use wasm_bindgen::prelude::*;
 
-use egui::{app, Context};
+use egui::{app, Context, Id};
 
 #[wasm_bindgen]
 pub fn start(canvas_id: &str) -> Result<(), wasm_bindgen::JsValue> {
@@ -26,6 +26,22 @@ impl app::App for WebApp {
         ctx: &Arc<Context>,
         integration_context: &mut app::IntegrationContext<'_>,
     ) {
+        egui::SidePanel::left(Id::new("side_panel"),200.0).show(ctx, |ui| {
+            ui.heading("Egui Demo");
+            ui.label("Egui is an immediate mode GUI library written in Rust.");
+            ui.add(egui::Hyperlink::new("https://github.com/emilk/egui").text("Egui home page"));
+
+            ui.separator();
+            ui.label(
+                "This is an example of a panel. Windows are constrained to the area that remain.",
+            );
+            if ui.button("Organize windows").clicked {
+                ui.ctx().memory().reset_areas();
+            }
+            ui.separator();
+
+            ui.heading("Windows:");
+        });
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("My Egui Application");
             ui.horizontal(|ui| {
